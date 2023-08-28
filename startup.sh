b27b616f119f76552b6e5d5d97729d6324edac21
bryan <74337078+bryan-l0@users.noreply.github.com>
2023-08-28 01:37:17 +0100

    Made more changes to startup script.

--- startup.sh
+++ startup.sh
@@ -1,10 +1,31 @@
 NOUT=/dev/null
 
-echo Installing packages...
-pip3 install alpaca-py > $NOUT
+if ! command -v python &> $NOUT; then
+  echo Python is not installed. Exiting.
+  exit
+elif ! command -v python &> $NOUT; then
+  echo Pip failed. Exiting.
+  exit
+fi
 
-echo Package installation complete!
+echo Checking packages are installed.
+
+installed_packages=$(pip list --format=columns | awk 'NR>2 {print $1 "==" $2}')
+while read -r package; do
+  if ! echo "$installed_packages" | grep -q "$package"; then
+    pip install --force-reinstall "$package" > $NOUT
+  fi
+done < dependencies
+
+echo Package validation complete.
+
+if ! [ -d dataset ]; then
+  echo Downloading dataset.
+  mkdir dataset
+  chmod +x ./bin/first_time.py
+  python ./bin/first_time.py
+fi
 
 chmod +x ./bin/main.py
 echo -e "\nRunning version $(cat version)\n"
-nohup python3 ./bin/main.py &
+nohup python ./bin/main.py &
